0847a597114b6df59e7a879b4d27d5f8b0fe2330
JeongHyeon Jo <abeul25@naver.com>
2024-10-31 01:27:37 +0900

    auth UI 개선

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:travel_on_final/features/auth/presentation/providers/auth_provider.dart';
 import 'package:travel_on_final/features/auth/presentation/widgets/text_field_widget.dart';
 import 'package:travel_on_final/features/auth/presentation/widgets/password_field_widget.dart';
@@ -14,6 +15,37 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isLoading = false;
+  bool _saveCredentials = false;
+  SharedPreferences? _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedCredentials();
+  }
+
+  // SharedPreferences에서 저장된 ID와 비밀번호를 불러오는 메소드
+  Future<void> _loadSavedCredentials() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _emailController.text = _prefs?.getString('savedEmail') ?? '';
+      _passwordController.text = _prefs?.getString('savedPassword') ?? '';
+      _saveCredentials = _prefs?.getBool('saveCredentials') ?? false;
+    });
+  }
+
+  // 로그인 시 ID와 비밀번호 저장
+  Future<void> _saveCredentialsToPrefs() async {
+    if (_saveCredentials) {
+      await _prefs?.setString('savedEmail', _emailController.text);
+      await _prefs?.setString('savedPassword', _passwordController.text);
+      await _prefs?.setBool('saveCredentials', _saveCredentials);
+    } else {
+      await _prefs?.remove('savedEmail');
+      await _prefs?.remove('savedPassword');
+      await _prefs?.remove('saveCredentials');
+    }
+  }
 
   Future<void> _login() async {
     setState(() => _isLoading = true);
@@ -24,6 +56,7 @@ class _LoginScreenState extends State<LoginScreen> {
     setState(() => _isLoading = false);
 
     if (authProvider.isAuthenticated) {
+      await _saveCredentialsToPrefs();
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('${authProvider.currentUser!.name}님 환영합니다.')),
       );
@@ -48,50 +81,71 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(title: Text('로그인')),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextFieldWidget(controller: _emailController, labelText: '이메일'),
-                  SizedBox(height: 16),
-                  PasswordFieldWidget(
-                    controller: _passwordController,
-                    labelText: '비밀번호',
-                  ),
-                  SizedBox(height: 24),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: _login,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[300],
-                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/travel-on-login.png',
+              height: 230,
+              width: 230,
+            ),
+            TextFieldWidget(controller: _emailController, labelText: '이메일'),
+            SizedBox(height: 20),
+            PasswordFieldWidget(
+              controller: _passwordController,
+              labelText: '비밀번호',
+            ),
+            SizedBox(height: 5),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Row(
+                    children: [
+                      Text('로그인 정보 저장'),
+                      Checkbox(
+                        value: _saveCredentials,
+                        onChanged: (value) {
+                          setState(() {
+                            _saveCredentials = value ?? false;
+                          });
+                        },
                       ),
-                      child: Text(
-                        '로그인',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: TextButton(
+                    onPressed: _navigateToSignup,
+                    child: Text(
+                      '회원가입',
+                      style: TextStyle(
+                        color: Colors.blue,
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Align(
-                    alignment: Alignment.bottomLeft,
-                    child: TextButton(
-                      onPressed: _navigateToSignup,
-                      child: Text('회원가입'),
-                    ),
-                  ),
-                  Consumer<AuthProvider>(
-                    builder: (context, auth, child) {
-                      return auth.isAuthenticated
-                          ? Text('Logged in as ${auth.currentUser?.name}')
-                          : Container();
-                    },
-                  ),
-                ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: _login,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[500],
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+                ),
+                child: Text(
+                  '로그인',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
