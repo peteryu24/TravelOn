e44da4713d5d369b11e9c73ba28f7b9a4a246702
JeongHyeon Jo <abeul25@naver.com>
2024-11-06 15:33:27 +0900

    fix: SafeArea 적용

--- lib/features/chat/presentation/screens/chat_screen.dart
+++ lib/features/chat/presentation/screens/chat_screen.dart
@@ -49,63 +49,65 @@ class _ChatScreenState extends State<ChatScreen> {
 
     return Scaffold(
       appBar: AppBar(title: Text("${otherUserName}님과의 대화")),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: true,
-              itemCount: chatProvider.messages.length,
-              itemBuilder: (ctx, index) {
-                final message = chatProvider.messages[index];
-                final isMe = message.uId == Provider.of<AuthProvider>(context, listen: false).currentUser!.id;
-                return MessageBubble(
-                  message: message,
-                  isMe: isMe,
-                  otherUserName: otherUserName,
-                );
-              },
-            ),
-          ),
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  _bottomSheetWidget.showBottomSheetMenu(
-                    parentContext: context,
-                    chatId: widget.chatId,
-                    userId: Provider.of<AuthProvider>(context, listen: false).currentUser!.id,
-                    otherUserId: otherUserId!,
-                    currentUserProfileImage: Provider.of<AuthProvider>(context, listen: false).currentUser!.profileImageUrl ?? '',
-                    username: Provider.of<AuthProvider>(context, listen: false).currentUser!.name,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                reverse: true,
+                itemCount: chatProvider.messages.length,
+                itemBuilder: (ctx, index) {
+                  final message = chatProvider.messages[index];
+                  final isMe = message.uId == Provider.of<AuthProvider>(context, listen: false).currentUser!.id;
+                  return MessageBubble(
+                    message: message,
+                    isMe: isMe,
+                    otherUserName: otherUserName,
                   );
                 },
               ),
-              Expanded(
-                child: TextField(
-                  controller: messageController,
-                  decoration: InputDecoration(
-                    hintText: '메시지를 입력하세요...',
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    _bottomSheetWidget.showBottomSheetMenu(
+                      parentContext: context,
+                      chatId: widget.chatId,
+                      userId: Provider.of<AuthProvider>(context, listen: false).currentUser!.id,
+                      otherUserId: otherUserId!,
+                      currentUserProfileImage: Provider.of<AuthProvider>(context, listen: false).currentUser!.profileImageUrl ?? '',
+                      username: Provider.of<AuthProvider>(context, listen: false).currentUser!.name,
+                    );
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: messageController,
+                    decoration: InputDecoration(
+                      hintText: '메시지를 입력하세요...',
+                    ),
                   ),
                 ),
-              ),
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  if (messageController.text.isNotEmpty) {
-                    chatProvider.sendMessageToChat(
-                      chatId: widget.chatId,
-                      text: messageController.text,
-                      otherUserId: otherUserId!,
-                      context: context,
-                    );
-                    messageController.clear();
-                  }
-                },
-              ),
-            ],
-          ),
-        ],
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    if (messageController.text.isNotEmpty) {
+                      chatProvider.sendMessageToChat(
+                        chatId: widget.chatId,
+                        text: messageController.text,
+                        otherUserId: otherUserId!,
+                        context: context,
+                      );
+                      messageController.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
