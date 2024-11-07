38b11e666e6d472344ea6fcb010f82f6f62e2087
zihoon9 <zihoon9@gmail.com>
2024-11-07 11:07:30 +0900

    design: 바텀 네비게이션 바 아이콘 수정

--- lib/features/home/presentation/widgets/bottom_navigation_bar.dart
+++ lib/features/home/presentation/widgets/bottom_navigation_bar.dart
@@ -1,7 +1,5 @@
-// lib/features/home/presentation/widgets/bottom_navigation_bar.dart
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int selectedIndex;
@@ -19,19 +17,19 @@ class CustomBottomNavigationBar extends StatelessWidget {
       type: BottomNavigationBarType.fixed,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
+          icon: Icon(CupertinoIcons.house_alt_fill),
           label: '홈',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.travel_explore),
+          icon: Icon(CupertinoIcons.compass_fill),
           label: '여행 상품',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.chat),
+          icon: Icon(CupertinoIcons.chat_bubble_2_fill),
           label: '채팅',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person),
+          icon: Icon(CupertinoIcons.person_crop_circle_fill),
           label: '마이페이지',
         ),
       ],
