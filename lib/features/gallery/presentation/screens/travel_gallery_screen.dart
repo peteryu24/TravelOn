8ecc34185b755e565adedf026d34efb3125670b2
zihoon9 <zihoon9@gmail.com>
2024-11-11 11:18:19 +0900

    refactor: 불필요한 import문 제거

--- lib/features/gallery/presentation/screens/travel_gallery_screen.dart
+++ lib/features/gallery/presentation/screens/travel_gallery_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../widgets/gallery_post.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
