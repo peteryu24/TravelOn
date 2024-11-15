5b1ab489399cd81a69d61beac9687a3444d942ac
JeongHyeon Jo <abeul25@naver.com>
2024-11-15 10:24:23 +0900

    fix: import 오류

--- lib/core/presentation/widgets/scaffold_with_bottom_nav.dart
+++ lib/core/presentation/widgets/scaffold_with_bottom_nav.dart
@@ -5,7 +5,7 @@ import 'package:travel_on_final/core/providers/navigation_provider.dart';
 import 'package:travel_on_final/features/chat/presentation/screens/chat_list_screen.dart';
 import 'package:travel_on_final/features/home/presentation/screens/home_screen.dart';
 import 'package:travel_on_final/features/home/presentation/widgets/bottom_navigation_bar.dart';
-import 'package:travel_on_final/features/profile/presentation/screens/profile_screen.dart';
+import 'package:travel_on_final/features/profile/presentation/screens/my_profile_screen.dart';
 import 'package:travel_on_final/features/search/presentation/screens/detail_screens.dart';
 
 class ScaffoldWithBottomNavBar extends StatelessWidget {
