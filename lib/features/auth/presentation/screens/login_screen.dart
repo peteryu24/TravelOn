2ccddc4f36de734eb2b97f9cc0490a958597f9d1
zihoon9 <zihoon9@gmail.com>
2024-11-15 18:11:49 +0900

    feat: 네이버 로그인 삭제

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -214,7 +214,129 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             SizedBox(height: 20.h),
+<<<<<<< Updated upstream
             _buildSocialButtons(),
+=======
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildSocialButton(
+                  iconWidget: ClipOval(
+                    child: Image.asset(
+                      'assets/images/google_light.png',
+                      width: 58.sp,
+                      height: 58.sp,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  color: Colors.white,
+                  onPressed: () async {
+                    try {
+                      final authProvider =
+                          Provider.of<AuthProvider>(context, listen: false);
+                      await authProvider.signInWithGoogle();
+
+                      if (authProvider.isAuthenticated) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  '${authProvider.currentUser!.name}님 환영합니다.')),
+                        );
+                        context.go('/');
+                      }
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Google 로그인에 실패했습니다.')),
+                      );
+                    }
+                  },
+                ),
+                SizedBox(width: 20.w),
+                _buildSocialButton(
+                  iconWidget: ClipOval(
+                    child: Image.asset(
+                      'assets/images/github_light.png',
+                      width: 34.sp,
+                      height: 34.sp,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  color: Colors.white,
+                  onPressed: () async {
+                    try {
+                      final authProvider =
+                          Provider.of<AuthProvider>(context, listen: false);
+                      await authProvider.signInWithGithub(context);
+
+                      if (authProvider.isAuthenticated) {
+                        if (!mounted) return;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  '${authProvider.currentUser!.name}님 환영합니다.')),
+                        );
+                        context.go('/');
+                      }
+                    } catch (e) {
+                      if (!mounted) return;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('GitHub 로그인에 실패했습니다.')),
+                      );
+                    }
+                  },
+                ),
+                // SizedBox(width: 20.w),
+                // _buildSocialButton(
+                //   iconWidget: ClipOval(
+                //     child: Image.asset(
+                //       'assets/images/naver_light.png',
+                //       width: 40.sp,
+                //       height: 40.sp,
+                //       fit: BoxFit.cover,
+                //     ),
+                //   ),
+                //   color: const Color(0xFF03C75A),
+                //   onPressed: () {
+                //     // Naver 로그인 구현
+                //   },
+                // ),
+                SizedBox(width: 20.w),
+                _buildSocialButton(
+                  iconWidget: ClipOval(
+                    child: Image.asset(
+                      'assets/images/kakao_light.png',
+                      width: 100.sp,
+                      height: 100.sp,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  color: const Color(0xFFFEE500),
+                  onPressed: () async {
+                    try {
+                      final authProvider =
+                          Provider.of<AuthProvider>(context, listen: false);
+                      await authProvider.signInWithKakao(context);
+
+                      if (authProvider.isAuthenticated) {
+                        if (!mounted) return;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text(
+                                  '${authProvider.currentUser!.name}님 환영합니다.')),
+                        );
+                        context.go('/');
+                      }
+                    } catch (e) {
+                      if (!mounted) return;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('카카오 로그인에 실패했습니다.')),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+>>>>>>> Stashed changes
             SizedBox(height: 30.h),
           ],
         ),
